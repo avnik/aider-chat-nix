f2752048223f3d0e669d7e2a309077f4723f7035
Alexander V. Nikolaev <avn@avnik.info>
2024-08-14 21:55:15 +0300

    Attempt to FIX
    Signed-off-by: Alexander V. Nikolaev <avn@avnik.info>

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+    inputs = {
+        flake-parts = {
+            url = "github:hercules-ci/flake-parts";
+            inputs.nixpkgs-lib.follows = "nixpkgs";
+        };
+        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+
+    outputs = inputs@{ flake-parts, nixpkgs, ...}:
+        flake-parts.lib.mkFlake
+        { inherit inputs; }
+        {
+            systems = [
+                "x86_64-linux"
+                "aarch64-linux"
+            ];
+
+            perSystem = { system, pkgs, ...}: {
+                packages = {
+                    default = let 
+                        aiderPython = pkgs.python312.override {
+                            packageOverrides = python-self: python-super: {
+                                grep-ast = python-self.callPackage ./grep-ast.nix {};
+                                tree-sitter = builtins.trace "FIXED" python-super.tree-sitter_0_21;
+                                tree-sitter-languages = builtins.trace "FIXXXED" python-super.tree-sitter-languages.override { tree-sitter = builtins.trace "${python-super.tree-sitter_0_21.version}" python-super.tree-sitter_0_21; };
+                            };
+                        };
+                        aider-chat = aiderPython.pkgs.callPackage ./aider-chat.nix {};
+                        in aider-chat;
+                };
+            };
+        };
+}
--- overlay.nix
+++ overlay.nix
@@ -2,6 +2,7 @@ self: super: {
   python3 = super.python3.override {
     packageOverrides = python-self: python-super: {
       grep-ast = python-self.callPackage ./grep-ast.nix {};
+      tree-sitter-languages = python-super.tree-sitter-languages { tree-sitter = python-super.tree-sitter_0_21; };
     };
   }; 
   aider-chat = super.callPackage ./aider-chat.nix {};
